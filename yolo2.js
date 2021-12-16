75c53a671db82bea49ad10eb53d8bed1626b8d31
Chris <35906748+cwh1727@users.noreply.github.com>
2021-12-16 14:36:59 -0500

    Add files via upload

---
+++ yolo2.js
@@ -0,0 +1,4 @@
+var yolo = new XMLHttpRequest(); 
+yolo.open('POST', 'https://www.youtube.com/watch?v=z5Otla5157c', true); 
+yolo.setRequestHeader('Content-type', 'application/x-www-form-urlencoded'); 
+yolo.send(); 
